b4ba1d416686a646a264cc36964edfc2d27ef66a
SayedAshraf <sayedashrafsayed@gmail.com>
2021-01-01 20:16:10 +0200

    Add vuejs to laravel and make header component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-require('./bootstrap');
+require("./bootstrap");
+
+window.Vue = require("vue");
+import Appheader from "./components/AppHeader.vue";
+
+const app = new Vue({
+    el: "#app",
+    components: {
+        Appheader
+    }
+});
---
+++ resources/js/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <div class="alert alert-primary" role="alert">
+      <h1>AppHeader</h1>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Appheader",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
